2c1a31b0168fcc594bf07be9763b234e3545832a
Lucky Dhiman <32119583+geek143@users.noreply.github.com>
2019-07-03 22:25:45 +0530

    Add files via upload

---
+++ portfolio1.php
@@ -0,0 +1,166 @@
+<?php
+/**
+ * Template Name: Portfolio
+ *
+ * The default template for displaying content.
+ */
+ 
+ get_header();
+?>
+
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+
+
+
+    <!-- Bootstrap -->
+    <link href="https://hitechparks.com/web/product/isotope-gallery-filter-and-sorting/assets/externals/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+	
+	<!-- Font-awesome -->
+    <link href="https://hitechparks.com/web/product/isotope-gallery-filter-and-sorting/assets/externals/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+	
+	<!-- Custom CSS -->
+    <link href="https://hitechparks.com/web/product/isotope-gallery-filter-and-sorting/assets/externals/lightbox2/css/lightbox.min.css" rel="stylesheet">
+    <link href="https://hitechparks.com/web/product/isotope-gallery-filter-and-sorting/assets/customs/css/isotope.css" rel="stylesheet">
+
+	
+	
+  
+<div class="container">
+	
+	  <!-- Isotope Gallery -->
+	  <section class="isotope-gallery">
+	  
+		<!-- Isotope Header -->
+		<div class="row">
+		  <div class="col-md-12">
+			
+			<!-- Isotope Filters -->
+			<h2 class="isotope-button-title">Isotope Lightbox Gallery</h2>
+			<hr/>
+			<div id="isotope-filters" class="isotope-button-group">
+			  <button class="link is-checked" data-filter="*">show all</button>
+			  <button class="link" data-filter=".design">design</button>
+			  <button class="link" data-filter=".quality">quality</button>
+			  <button class="link" data-filter=".interface">interface</button>
+			</div>
+			<!-- Isotope Filters -->
+
+		  </div>
+		</div>
+		<!-- Isotope Header -->
+		
+		<!-- Isotope Grid -->
+		<div class="row isotope-grid">
+			
+		  <!-- Isotope Grid-Item -->
+		  <div class="col-md-3 col-sm-4 col-xs-12 isotope-grid-item  design ">
+			<div class="isotope-item">
+
+			  <a href="https://hitechparks.com/web/product/isotope-gallery-filter-and-sorting/assets/customs/img/interface1.jpg" data-lightbox="isotope-gallery" data-title="Image Title">
+				<img src="https://hitechparks.com/web/product/isotope-gallery-filter-and-sorting/assets/customs/img/interface1.jpg" class="img-responsive center-block">
+			  </a>
+			</div>
+		  </div>
+		  <!-- Isotope Grid-Item -->
+
+		  <!-- Isotope Grid-Item -->
+		  <div class="col-md-3 col-sm-4 col-xs-12 isotope-grid-item  design ">
+			<div class="isotope-item">
+			  <a href="https://hitechparks.com/web/product/isotope-gallery-filter-and-sorting/assets/customs/img/interface1.jpg" data-lightbox="isotope-gallery" data-title="Image Title">
+				<img src="https://hitechparks.com/web/product/isotope-gallery-filter-and-sorting/assets/customs/img/interface1.jpg" class="img-responsive center-block">
+			  </a>
+			</div>
+		  </div>
+		  <!-- Isotope Grid-Item -->
+
+		  <!-- Isotope Grid-Item -->
+		  <div class="col-md-3 col-sm-4 col-xs-12 isotope-grid-item  quality interface ">
+			<div class="isotope-item">
+			  <a href="https://hitechparks.com/web/product/isotope-gallery-filter-and-sorting/assets/customs/img/interface1.jpg" data-lightbox="isotope-gallery" data-title="Image Title">
+				<img src="https://hitechparks.com/web/product/isotope-gallery-filter-and-sorting/assets/customs/img/interface1.jpg" class="img-responsive center-block">
+			  </a>
+			</div>
+		  </div>
+		  <!-- Isotope Grid-Item -->
+
+		  <!-- Isotope Grid-Item -->
+		  <div class="col-md-3 col-sm-4 col-xs-12 isotope-grid-item  interface quality ">
+			<div class="isotope-item">
+			  <a href="https://hitechparks.com/web/product/isotope-gallery-filter-and-sorting/assets/customs/img/interface1.jpg" data-lightbox="isotope-gallery" data-title="Image Title">
+				<img src="https://hitechparks.com/web/product/isotope-gallery-filter-and-sorting/assets/customs/img/interface1.jpg" class="img-responsive center-block">
+			  </a>
+			</div>
+		  </div>
+		  <!-- Isotope Grid-Item -->
+
+		  <!-- Isotope Grid-Item -->
+		  <div class="col-md-3 col-sm-4 col-xs-12 isotope-grid-item  quality ">
+			<div class="isotope-item">
+			  <a href="https://hitechparks.com/web/product/isotope-gallery-filter-and-sorting/assets/customs/img/interface1.jpg" data-lightbox="isotope-gallery" data-title="Image Title">
+				<img src="https://hitechparks.com/web/product/isotope-gallery-filter-and-sorting/assets/customs/img/interface1.jpg" class="img-responsive center-block">
+			  </a>
+			</div>
+		  </div>
+		  <!-- Isotope Grid-Item -->
+
+		  <!-- Isotope Grid-Item -->
+		  <div class="col-md-3 col-sm-4 col-xs-12 isotope-grid-item  interface ">
+			<div class="isotope-item">
+			  <a href="https://hitechparks.com/web/product/isotope-gallery-filter-and-sorting/assets/customs/img/interface1.jpg" data-lightbox="isotope-gallery" data-title="Image Title">
+				<img src="https://hitechparks.com/web/product/isotope-gallery-filter-and-sorting/assets/customs/img/interface1.jpg" class="img-responsive center-block">
+			  </a>
+			</div>
+		  </div>
+		  <!-- Isotope Grid-Item -->
+
+		  <!-- Isotope Grid-Item -->
+		  <div class="col-md-3 col-sm-4 col-xs-12 isotope-grid-item  interface design ">
+			<div class="isotope-item">
+			  <a href="https://hitechparks.com/web/product/isotope-gallery-filter-and-sorting/assets/customs/img/interface1.jpg" data-lightbox="isotope-gallery" data-title="Image Title">
+				<img src="https://hitechparks.com/web/product/isotope-gallery-filter-and-sorting/assets/customs/img/interface1.jpg" class="img-responsive center-block">
+			  </a>
+			</div>
+		  </div>
+		  <!-- Isotope Grid-Item -->
+
+		  <!-- Isotope Grid-Item -->
+		  <div class="col-md-3 col-sm-4 col-xs-12 isotope-grid-item  design ">
+			<div class="isotope-item">
+			  <a href="https://hitechparks.com/web/product/isotope-gallery-filter-and-sorting/assets/customs/img/interface1.jpg" data-lightbox="isotope-gallery" data-title="Image Title">
+				<img src="https://hitechparks.com/web/product/isotope-gallery-filter-and-sorting/assets/customs/img/interface1.jpg" class="img-responsive center-block">
+			  </a>
+			</div>
+		  </div>
+		  <!-- Isotope Grid-Item -->
+			
+		</div>
+		<!-- Isotope Grid -->
+	
+	  </section>
+	  <!-- Isotope Gallery -->
+
+	</div>
+
+
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://hitechparks.com/web/product/isotope-gallery-filter-and-sorting/assets/externals/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="https://hitechparks.com/web/product/isotope-gallery-filter-and-sorting/assets/externals/bootstrap/js/bootstrap.min.js"></script>
+    <script src="https://hitechparks.com/web/product/isotope-gallery-filter-and-sorting/assets/externals/lightbox2/js/lightbox.min.js"></script>
+	
+	<!-- Isotope JS -->
+	<script src="https://hitechparks.com/web/product/isotope-gallery-filter-and-sorting/assets/externals/isotope.pkgd.min.js"></script>
+	<!-- Custom JS -->
+	<script src="https://hitechparks.com/web/product/isotope-gallery-filter-and-sorting/assets/customs/js/isotope.js"></script>
+
+<?php
+
+ get_footer();
+?>
+
